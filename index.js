aea29655d51731baab3bed4cee55cb2392436fc9
Pavan5179 <110727305+Pavan5179@users.noreply.github.com>
2022-09-07 14:08:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const server = express()
+const fetch = require("node-fetch")
+
+
+const getJSON = async (id) => {
+    const redirect = await fetch(`https://fembed.net/v/${id}`).then(res => res.url.replace("/v/", "/api/source/"))
+    const video = await fetch(redirect, { method: "POST" }).then(res => res.json())
+    if (!video["success"]) return { success: false }
+    return video
+}
+
+
+const getVideoURL = async (videoJSON) => {
+    const data = []
+    for (filedata of videoJSON) {
+        data.push({
+            file: filedata["file"],
+            label: filedata["label"],
+            type: filedata["type"]
+        })
+    }
+    return data
+}
+
+server.get("/:id", async (req, res) => {
+    const id = req.params.id
+    const videoJSON = await getJSON(id)
+    if (!videoJSON["success"]) return res.status(404).json({ success: false })
+    return res.json(videoJSON["data"])
+})
+
+server.listen(3000)
+
